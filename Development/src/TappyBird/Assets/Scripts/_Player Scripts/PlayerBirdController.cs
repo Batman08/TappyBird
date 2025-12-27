730ddc7c1b1a75575b46f9d7750d4473e7cef581
bilal asghar <asgharbilal8@gmail.com>
2025-12-27 15:14:32 +0000

    remove unused using statements from player controller script

--- Development/src/TappyBird/Assets/Scripts/_Player Scripts/PlayerBirdController.cs
+++ Development/src/TappyBird/Assets/Scripts/_Player Scripts/PlayerBirdController.cs
@@ -1,8 +1,5 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.SceneManagement;
 using UnityEngine;
-using System;
 
 public class PlayerBirdController : MonoBehaviour
 {
