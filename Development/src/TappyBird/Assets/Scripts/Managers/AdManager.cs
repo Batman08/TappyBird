0e0749a88642cd572975966fb967b7dc63cab734
bilal asghar <asgharbilal8@gmail.com>
2026-04-24 18:19:14 +0100

    implement ad manager script

---
+++ Development/src/TappyBird/Assets/Scripts/Managers/AdManager.cs
@@ -0,0 +1,136 @@
+using GoogleMobileAds.Api;
+using System;
+using UnityEngine;
+
+public class AdManager : MonoBehaviour
+{
+    public static AdManager Instance { get; private set; }
+
+
+    public event Action<Reward> OnShowRewardedAdCompleted;
+
+#if UNITY_EDITOR
+    private readonly string TappyBird_KeepPlaying_Reward_Unit_Id = "ca-app-pub-3940256099942544/5224354917";
+#elif UNITY_ANDROID
+    private readonly string TappyBird_KeepPlaying_Reward_Unit_Id = "";
+#endif
+    private RewardedAd _rewardedAd;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        InitMobileAds();
+    }
+
+
+    #region InitMobileAds
+
+    private void InitMobileAds()
+    {
+        // Initialize Google Mobile Ads Unity Plugin.
+        MobileAds.Initialize((InitializationStatus initStatus) =>
+        {
+            LoadRewardedAd();
+            Log.Info("Mobile Ads SDK initialized.");
+        });
+    }
+
+    #endregion
+
+
+    #region LoadRewardedAd
+
+    private void LoadRewardedAd()
+    {
+        // If we already have a rewarded ad, destroy it and create a new one.
+        DestroyAd();
+
+        // Create our request used to load the ad.
+        var adRequest = new AdRequest();
+
+        // Send the request to load the ad.
+        RewardedAd.Load(TappyBird_KeepPlaying_Reward_Unit_Id, adRequest, (RewardedAd ad, LoadAdError error) =>
+        {
+            Log.Info("Rewarded ad load callback received.");
+
+            bool adLoadedSuccessfully = ad != null;
+            if (adLoadedSuccessfully)
+            {
+                _rewardedAd = ad;
+                EventListeners();
+
+                Log.Info("Rewarded ad loaded successfully.");
+            }
+            else
+            {
+                Log.Error($"Rewarded ad failed to load with error: {error.GetMessage()}");
+            }
+        });
+    }
+
+    #endregion
+
+
+    #region ShowRewardedAd
+
+    public void ShowRewardedAd()
+    {
+        Log.Info("Attempting to show rewarded ad.");
+        if (_rewardedAd != null && _rewardedAd.CanShowAd())
+        {
+            Log.Info("Rewarded ad is ready to be shown. Showing ad now.");
+            _rewardedAd.Show((Reward reward) =>
+            {
+                Log.Info($"Reward received: {reward.Type}, amount: {reward.Amount}");
+
+                // The ad was showen and the user earned a reward.
+                OnShowRewardedAdCompleted?.Invoke(reward);
+            });
+        }
+        else
+        {
+            Log.Warning("Rewarded ad is not ready yet. Please try again later.");
+        }
+    }
+
+    #endregion
+
+
+    #region EventListeners
+
+    private void EventListeners()
+    {
+        // Raised when the ad closed full screen content.
+        _rewardedAd.OnAdFullScreenContentClosed += OnAdFullScreenContentClosed_ReloadAd;
+    }
+
+    private void OnAdFullScreenContentClosed_ReloadAd()
+    {
+        // Reload the ad so that we can show another as soon as possible.
+        LoadRewardedAd();
+    }
+
+    #endregion
+
+
+    #region DestroyAd
+
+    private void DestroyAd()
+    {
+        _rewardedAd?.Destroy();
+    }
+
+    #endregion
+}
